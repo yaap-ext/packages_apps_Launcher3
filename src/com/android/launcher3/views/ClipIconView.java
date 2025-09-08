554dc46cdb3ca6a6e95fa849f0502de21f005d16
Dmitrii <bankersenator@gmail.com>
2025-09-08 17:29:00 +0000

    Launcher3: fix crash in ClipIconView
    java.lang.NullPointerException: Attempt to invoke virtual method 'void android.graphics.drawable.Drawable.setBounds(android.graphics.Rect)' on a null object reference
    	at com.android.launcher3.views.ClipIconView.setBackgroundDrawableBounds(ClipIconView.java:223)
    	at com.android.launcher3.views.ClipIconView.update(ClipIconView.java:197)
    	at com.android.launcher3.views.ClipIconView.update(ClipIconView.java:153)
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/views/ClipIconView.java
+++ src/com/android/launcher3/views/ClipIconView.java
@@ -219,7 +219,9 @@ public class ClipIconView extends View implements ClipPathView {
         } else {
             sTmpRect.offsetTo(sTmpRect.left, (int) (mFinalDrawableBounds.top * scale));
         }
+        if (mBackground != null) {
         mBackground.setBounds(sTmpRect);
+        }
     }
 
     protected void endReveal() {
