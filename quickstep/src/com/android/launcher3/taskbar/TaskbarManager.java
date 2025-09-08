3d2862dd67b76b49c53cae9296a6f11eee6cedf4
Dmitrii <bankersenator@gmail.com>
2025-09-08 17:34:02 +0000

    Launcher3: Ensure taskbar destruction happens on the main thread
    java.lang.IllegalStateException: Method handleLifecycleEvent must be called on the main thread
    	at androidx.lifecycle.LifecycleRegistry.enforceMainThreadIfNeeded(LifecycleRegistry.jvm.kt:297)
    	at androidx.lifecycle.LifecycleRegistry.handleLifecycleEvent(LifecycleRegistry.jvm.kt:118)
    	at com.android.launcher3.util.BaseContext.onViewDestroyed(BaseContext.kt:131)
    	at com.android.launcher3.taskbar.TaskbarActivityContext.onDestroy(TaskbarActivityContext.java:1035)
    	at com.android.launcher3.taskbar.TaskbarManager.destroyTaskbarForDisplay(TaskbarManager.java:600)
    	at com.android.launcher3.taskbar.TaskbarManager.destroyAllTaskbars(TaskbarManager.java:588)
    	at com.android.launcher3.taskbar.TaskbarManager.lambda$new$2(TaskbarManager.java:520)
    	at com.android.launcher3.taskbar.TaskbarManager.$r8$lambda$74dnLrKVAhjdHODkpHVex7T3uw4(TaskbarManager.java:0)
    	at com.android.launcher3.taskbar.TaskbarManager$$ExternalSyntheticLambda3.accept(R8$$SyntheticClass:0)
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -482,9 +482,11 @@ public class TaskbarManager implements DisplayDecorationListener {
 
         SystemDecorationChangeObserver.getINSTANCE().get(mPrimaryWindowContext)
                 .registerDisplayDecorationListener(this);
-        mShutdownReceiver =
+	mShutdownReceiver =
                 new SimpleBroadcastReceiver(
-                        mPrimaryWindowContext, UI_HELPER_EXECUTOR, i -> destroyAllTaskbars());
+                        mPrimaryWindowContext,
+                        UI_HELPER_EXECUTOR,
+                        i -> MAIN_EXECUTOR.execute(this::destroyAllTaskbars));
         mTaskbarBroadcastReceiver =
                 new SimpleBroadcastReceiver(mPrimaryWindowContext,
                         UI_HELPER_EXECUTOR, this::showTaskbarFromBroadcast);
